3f0c0b8ada7f1ff3131f3acc83951f0464683ab1
Joshua V Arnaez <jarnaez@yondu.com>
2022-08-10 09:48:15 +0800

    ...

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
 import type { ReactElement } from 'react';
 import Login from '@/components/Login';
 import Layout from '@/components/Layout';
+import { Head } from 'next/document';
 
 export default function Home() {
-  return <Login />;
+  return (
+    <>
+      <Head>
+        <title>Web-app</title>
+        <meta name='description' content='Sort description of your website' />
+        <meta name='keywords' content='keyword1,keyword2,...' />
+        <meta name='og:title' property='og:title' content='Your Open Graph Title' />
+        <meta name='robots' content='index, follow' />
+        <link href='URL' rel='canonical' />
+      </Head>
+      <Login />
+    </>
+  );
 }
 
 // Home.getLayout = function getLayout(page: ReactElement) {
