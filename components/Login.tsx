3e50e83a7ada4a36c8f2d54fa3dc21357776ac93
Joshua V Arnaez <jarnaez@yondu.com>
2022-08-10 11:54:03 +0800

    ..

--- components/Login.tsx
+++ components/Login.tsx
@@ -4,25 +4,29 @@ import React from 'react';
 export default function Login() {
   return (
     <Flex w='100%' h='100vh'>
-      <Flex
-        w={{ base: '100%', lg: '50%' }}
-        bg='white'
-        h='100%'
-        alignItems='center'
-      >
-        <Flex boxShadow='md' rounded='md' w={{base:'90%', md: '80%'}} maxW='500px' mx='auto' bg='#3b9ae3' direction='column' p='10' gap='4'>
+      <Flex w={{ base: '100%', lg: '50%' }} bg='white' h='100%' alignItems='center'>
+        <Flex
+          boxShadow='md'
+          rounded='md'
+          w={{ base: '90%', md: '80%' }}
+          maxW='500px'
+          mx='auto'
+          bg='#3b9ae3'
+          direction='column'
+          p='10'
+          gap='4'
+        >
           <Text fontSize='SubHeader.lg' textAlign='start'>
             Login
           </Text>
           <Input placeholder='Username' bg='white' type='text' />
           <Input placeholder='Password' bg='white' type='password' />
-          <Button bg='blue' borderRadius='20px' border='none' color='white' _hover={{color: 'blue', bg:'white'}}>
+          <Button bg='blue' borderRadius='20px' border='none' color='white' _hover={{ color: 'blue', bg: 'white' }}>
             Login
           </Button>
         </Flex>
       </Flex>
-      <Box w={{ base: '0%', lg: '50%' }} bg='gray' h='100%'>
-      </Box>
+      <Box w={{ base: '0%', lg: '50%' }} bg='gray' h='100%'></Box>
     </Flex>
   );
 }
